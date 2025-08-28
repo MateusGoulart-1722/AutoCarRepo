c6a228e9d7234d0a96b29d8326ce2233cb9c2d85
MateusGoulart-1722 <mateus.goulart@etec.sp.gov.br>
2025-08-28 12:26:44 -0300

    criado contrutor vaio

--- src/pasta/ClasseAutomotores.java
+++ src/pasta/ClasseAutomotores.java
@@ -3,7 +3,13 @@ package pasta;
 public class ClasseAutomotores {
 
 	public ClasseAutomotores() {
+		String corAt;
+		String marcaAt;
+		String modeloAt;
+		String tipoAt;
+		
 		// TODO Auto-generated constructor stub
 	}
 
+	public ClasseAutomotores () {}
 }
